4493391d5b9f496bb325681869c932ec78389c4c
Franklin Perez <franklin9perez952012@gmail.com>
2019-02-14 13:17:33 -0600

    frank

---
+++ frank/franklin.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>practica git</title>
+</head>
+<body>
+<h1>git 2019</h1>
+<?php
+print (“<h2>Practica git</h2>”);
+?>
+<p>Hola que hace</p>
+<?php
+print (“<p>phpmyadmin.</p>”);
+?>
+
+</body>
+</html>
