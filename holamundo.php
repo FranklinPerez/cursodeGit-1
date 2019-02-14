a5eeb8fc0f0ca2273899a1f9ac31ca634265857a
Edwin Otoniel Guzman Melara <gm12006@ues.edu.sv>
2019-02-14 12:11:09 -0600

    Revert "ESTO VOY A REVERTIR"
    This reverts commit 2b527823ae83d3f47cd4246df36507f68851c90d.

--- holamundo.php
+++ holamundo.php
@@ -1,6 +1,2 @@
 <?php
 @print "Hola {$argv[1]}\n";
-@print "Hola {$argv[1]}\n";@print "Hola {$argv[1]}\n";@print "Hola {$argv[1]}\n";@print "Hola {$argv[1]}\n";@print "Hola {$argv[1]}\n";
-@print "Hola {$argv[1]}\n";@print "Hola {$argv[1]}\n";
-
-@print "Hola {$argv[1]}\n";
